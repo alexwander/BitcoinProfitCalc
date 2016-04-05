c2752b053e02f18570b226f93e363cb3cb1e75b1
alexwander <ryndyuk.alexander@gmail.com>
2016-04-05 20:46:41 +0300

    clean up

--- server.js
+++ server.js
@@ -6,41 +6,7 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var config = require('./config');
 var mongoose = require('mongoose');
-var app = express();
 
 
-var app = express();
-
-var port = process.env.PORT || 5000;
-var nav = [{
-    Link: '/Books',
-    Text: 'Book'
-}, {
-    Link: '/Authors',
-    Text: 'Author'
-}];
-var bookRouter = require('./src/routes/bookRoutes')(nav);
-var adminRouter = require('./src/routes/adminRoutes')(nav);
-var authRouter = require('./src/routes/authRoutes')(nav);
 
 
-app.get('/', function (req, res) {
-    res.render('index', {
-        title: 'Hello from render',
-        nav: [{
-            Link: '/Books',
-            Text: 'Books'
-        }, {
-            Link: '/Authors',
-            Text: 'Authors'
-        }]
-    });
-});
-
-app.get('/books', function (req, res) {
-    res.send('Hello Books');
-});
-
-app.listen(port, function (err) {
-    console.log('running server on port ' + port);
-});
