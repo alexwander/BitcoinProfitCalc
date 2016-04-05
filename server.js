abc93c9d46aba99fe0af16412e7a2fbd97cdce25
alexwander <ryndyuk.alexander@gmail.com>
2016-04-05 20:37:02 +0300

    conf server

--- server.js
+++ server.js
@@ -7,3 +7,18 @@ var morgan = require('morgan');
 var config = require('./config');
 var mongoose = require('mongoose');
 var app = express();
+
+
+var app = express();
+
+var port = process.env.PORT || 5000;
+var nav = [{
+    Link: '/Books',
+    Text: 'Book'
+}, {
+    Link: '/Authors',
+    Text: 'Author'
+}];
+var bookRouter = require('./src/routes/bookRoutes')(nav);
+var adminRouter = require('./src/routes/adminRoutes')(nav);
+var authRouter = require('./src/routes/authRoutes')(nav);
