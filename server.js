4b3ff1da6f36c82166aab4075fa802e71cd24f05
alexwander <ryndyuk.alexander@gmail.com>
2016-04-05 20:33:06 +0300

    upd server

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 /**
  * Created by alexandr on 05.04.2016.
  */
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var config = require('./config');
+var mongoose = require('mongoose');
+var app = express();
