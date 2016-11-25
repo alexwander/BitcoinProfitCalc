15d494894438dd163f22865ceae8daec88450a9b
alexwander <ryndyuk.alexander@gmail.com>
2016-11-25 16:10:41 +0200

    add ng material theme conf

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
 angular
     .module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
     .controller('DemoCtrl', function($scope, $http) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+        .config(function($mdThemingProvider) {
+
+            // Configure a dark theme with primary foreground yellow
+
+            $mdThemingProvider.theme('docs-dark', 'default')
+                .primaryPalette('yellow')
+                .dark();
+
+            $mdThemingProvider.theme('docs-light', 'default')
+                .primaryPalette('blue')
+                .accentPalette('green');
+
+
+        });
